5be2fe689ec3c6a2671d9bad8ffd91eaa008851f
Perchun_Pak <perchunpak@gmail.com>
2021-08-19 23:45:05 +0300

    Улучшил немного

--- main.py
+++ main.py
@@ -1,52 +1,45 @@
-import proxycheck
+"""
+Если честно я сам не до конца понимаю что тут хотел сделать автор, однако явно он эксперементировал
+с модулем proxycheck, останется в репозитории скорее всего просто чтобы было
+"""
+from proxycheck import Blocking
 import geoip2.database
 
-# IPS to check
 ips_to_check = ['1.1.1.1', '8.8.8.8']
 ips_found = []
 
-# Config
-apitoken = '' # от proxycheck.io
+PROXYCHECK_IO_TOKEN = ''
 
-# Code
-client = proxycheck.Blocking(
-    key=apitoken
+client = Blocking(
+    key=PROXYCHECK_IO_TOKEN
 )
 
 for checkip in ips_to_check:
-    print('Check ' + checkip)
+    print('Проверяю ' + checkip)
     ip = client.ip(checkip)
     risk = ip.risk()
-    print('IPs risk: ' + str(risk))
+    print('Риск IP: ' + str(risk))
     if risk == 100:
-        ips_found.append(f"IP's risk: {str(risk)} Checking IP: {checkip} Is proxy: {ip.proxy()}")
-        break
-    else:
-        ips_found.append(str(risk))
+        ips_found.append(f"Риск IP: {str(risk)} у IP {checkip}, прокси: {ip.proxy()}")
 
-    #if ip.risk():
-    #    print('Find ' + checkip)
-    #    ips_found.append(checkip)
+    if risk:
+       print('Найдено ' + checkip)
+       ips_found.append(checkip)
 
-    risk_score = ip.risk()
-    latitude, longitude = ip.geological()
-
-    data = ip.get()
-
-    # A client should always be closed after being used!
 print(ips_found)
 client.close()
 
 
+ips_found = []
 
-#with geoip2.database.Reader('./proxycheck/GeoIP2-City.mmdb') as reader:
-#    for checkip in ips_to_check:
-#        response = reader.anonymous_ip(checkip)
-#        ips_found.append(f'is_anonymous:{response.is_anonymous}'
-#                         f'is_anonymous_vpn:{response.is_anonymous_vpn}'
-#                         f'is_hosting_provider:{response.is_hosting_provider}'
-#                         f'is_public_proxy:{response.is_public_proxy}'
-#                         f'is_residential_proxy:{response.is_residential_proxy}'
-#                         f'is_tor_exit_node:{response.is_tor_exit_node}'
-#                         f'ip_address:{response.ip_address}')
-#print(ips_found)
+with geoip2.database.Reader('./proxycheck/GeoIP2-City.mmdb') as reader:
+   for checkip in ips_to_check:
+       response = reader.anonymous_ip(checkip)
+       ips_found.append(f'is_anonymous:{response.is_anonymous}'
+                        f'is_anonymous_vpn:{response.is_anonymous_vpn}'
+                        f'is_hosting_provider:{response.is_hosting_provider}'
+                        f'is_public_proxy:{response.is_public_proxy}'
+                        f'is_residential_proxy:{response.is_residential_proxy}'
+                        f'is_tor_exit_node:{response.is_tor_exit_node}'
+                        f'ip_address:{response.ip_address}')
+print(ips_found)
